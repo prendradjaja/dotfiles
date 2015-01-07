922f36030c99bd371b3ab83c803c9c927d9a9565
Pandu <prendradjaja@gmail.com>
2015-01-06 20:50:28 -0800

    Add inputrc

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/bashrc/main.sh ~/.bashrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimperatorrc ~/.vimperatorrc
+ln -s ~/dotfiles/inputrc ~/.inputrc
