ea3b965d2f1d96af4ee22c8b54ccb4b7f5eff35f
Pandu <prendradjaja@gmail.com>
2015-01-06 20:50:15 -0800

    Update bashrc link in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/bashrc/main.sh ~/.bashrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimperatorrc ~/.vimperatorrc
