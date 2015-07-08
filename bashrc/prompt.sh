0c35dd18395c43dfd5d04ac93992c528accd939a
vagrant <prendradjaja@gmail.com>
2015-07-08 14:42:27 +0000

    Add no-shorten newline prompt

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -3,6 +3,9 @@ PROMPT_COMMAND='DIR=`pwd|sed -e "s!$HOME!~!"`; if [ ${#DIR} -gt 28 ]; then CurDi
 # No hostname
 PS1='\j \[\033[1m\]${CurDir}\[\033[0m\] '
 
+# No shortening, newline
+# PS1='\n\j \[\033[1m\]\w\[\033[0m\]\n> '
+
 # With hostname
 # PS1='\j [\h] \[\033[1m\]${CurDir}\[\033[0m\] '
 
