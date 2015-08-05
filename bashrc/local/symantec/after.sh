cb467ac824774d3cc0582441b5ac7569bd001b69
vagrant <prendradjaja@gmail.com>
2015-08-05 19:06:43 +0000

    symantec modifications

--- bashrc/local/symantec/after.sh
+++ bashrc/local/symantec/after.sh
@@ -6,3 +6,12 @@ unset -f gvim
 
 pf() { cd /build/Perforce/$1 && l; }
 source ~/dotfiles/bashrc/local/symantec/_pf.sh
+
+# lynx-curl
+lcurl() { curl --silent $1 | lynx -stdin -dump; }
+
+# protoc
+alias protoc='LD_LIBRARY_PATH=/home/vagrant/tools/proto/lib/ /home/vagrant/tools/proto/protoc'
+
+# mts-console
+alias mts='/build/Perforce/Pandu_Rendradjaja_vm_mts/dev/dist/tools/mts-client/bin/mts-client -m localhost -a https://login-int2.norton.com/sso -l "foobar@foo.com" -p "htns&8" --console'
